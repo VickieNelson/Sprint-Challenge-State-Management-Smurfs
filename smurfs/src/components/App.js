ec21746ad51f25e13f936fc2f0bde52fa2f6b738
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-06-12 11:10:20 -0500

    conext created, imported and wrapped

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
+import SmurfsContext from "../contexts/SmurfsContext";
 import "./App.css";
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+      <div className='App'>
+        <SmurfsContext.Provider value={{ smurfs }}>
+          <h1>SMURFS! 2.0 W/ Redux</h1>
+          <div>Welcome to your state management version of Smurfs!</div>
+          <div>Start inside of your `src/index.js` file!</div>
+          <div>Have fun!</div>
+        </SmurfsContext.Provider>
       </div>
     );
   }
